16f059a500d91672ff9357856b27f6d9bd90c4b8
Matthew Hyndman <myndman@gmail.com>
2015-03-17 23:46:09 -0700

    added a sanity test

--- tsw/tests.py
+++ tsw/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from tsw.models import *
+from tsw.views import *
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+
+import json
+
+class SanityTest(TestCase):
+    def setup(self):
+        self.client = Client()
+
+    def test_server_info(self):
+        params = {'domain': 'test.com'}
+        response = self.client.get(reverse('tsw.views.server_info'), params)
+        assert response.status_code == 200
+        data = json.loads(response.content)
+        assert data['swf_url']
+        assert data['base_url']
+
+    def test_new_user(self):
+        params = {'name': ''}
+        response = self.client.get(reverse('tsw.views.new_user'), params)
+        assert response.status_code == 200
+        data = json.loads(response.content)
+        assert data['name'][:4] == 'Anon'
+        assert data['user_id']
+        assert data['secret_code']
+        assert data['create_date']
+
+# future tests: date filters, sorting, get scores cases, w/e
+# also test unicode and long fields/missing malformed fields
+
+# verify side effects (metrics, objects)
+
